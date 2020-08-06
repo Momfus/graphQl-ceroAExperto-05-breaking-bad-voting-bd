f7acef063d2965851b59ae524b0a0e04000ad3d0
Momfus <momfus@outlook.com>
2020-08-06 03:14:50 -0300

    agregdo los mutations necesarios - Clase 197

--- src/lib/database-operations.ts
+++ src/lib/database-operations.ts
@@ -11,6 +11,14 @@ export async function getCharacters( db: any )  {
 }
 
 
+// Voto seleccionado
+export async function getVote( db: any, id: string ) {
+
+    return await db.collection(COLLECTIONS.VOTES).findOne({ id });
+
+}
+
+
 // Personaje seleccionado (solo uno)
 export async function getCharacter( db: any, id: string ) {
 
@@ -23,5 +31,21 @@ export async function getCharacter( db: any, id: string ) {
 export async function getCharacterVotes( db: any, id: string ) {
 
     return await db.collection(COLLECTIONS.VOTES).find({ character: id }).count(); // Saber cuantos hay del id señalado
+
+}
+
+// Obtener el id del nuevo voto
+export async function assignVoteId( db: any ) {
+
+    const lastVotes = await db.collection( COLLECTIONS.VOTES )
+                            .find().sort( { _id: -1 } ).limit(1).toArray() // Del orden de abajo hacia arriba
+
+    if( lastVotes.length === 0 ){
+
+        return "1";
+
+    }
+
+    return String( +lastVotes[0].id + 1 ); // Añadir su id un número superior al último que habia (siendo así único)
 
 }
--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -1,7 +1,185 @@
 
 import { IResolvers } from 'graphql-tools';
+import { getCharacter, getCharacters, assignVoteId, getVote } from '../lib/database-operations';
+import { Datetime } from '../lib/datetime';
+import { COLLECTIONS } from '../config/constants';
 
 const mutation: IResolvers = {
+
+    Mutation: {
+
+        async addVote( _: void, { character }, { db } ) {
+
+            // Comprobar que el personaje existe
+            const selectCharacter = await getCharacter( db, character );
+            
+            if( selectCharacter === null || selectCharacter === undefined )  {
+
+                return {
+
+                    status: false,
+                    message: 'El personaje introducido no existe y no puedes votar',
+                    characters: await getCharacters( db )
+
+                }
+
+            }
+
+ 
+            // Obtener id del voto
+            const vote = {
+                id: await assignVoteId(db),
+                character,
+                createdAt : new Datetime().getCurrentDateTime()
+            };
+
+            // En caso de existir
+            return await db.collection( COLLECTIONS.VOTES ).insertOne(vote).then(
+
+                // Añadir a la colección el nuevo voto
+                async() => {
+                    return {
+        
+                        status: true,
+                        message: 'El personaje existe y se ha emitido correctamente',
+                        characters: await getCharacters( db )
+        
+                    }
+
+                }
+
+            ).catch(
+
+                // Capturar error
+                async() => {
+                    return {
+        
+                        status: false,
+                        message: 'El voto NO se ha emitido. Prueba de nuevo.',
+                        characters: await getCharacters( db )
+        
+                    }
+
+                }
+
+            )
+            
+
+        },
+
+        async updateVote( _: void, {id, character }, { db } ) {
+
+            // Comprobar que el personaje existe
+            const selectCharacter = await getCharacter( db, character );
+            
+            if( selectCharacter === null || selectCharacter === undefined )  {
+
+                return {
+
+                    status: false,
+                    message: 'El personaje introducido no existe y no puedes actualizar el voto',
+                    characters: await getCharacters( db )
+
+                }
+
+            }
+
+            // Comprobar que el voto existe
+            const selectVote = await getVote( db, id );
+            
+            if( selectVote === null || selectVote === undefined )  {
+
+                return {
+
+                    status: false,
+                    message: 'El voto introducido no existe y no puedes actualizar',
+                    characters: await getCharacters( db )
+
+                }
+
+            }
+
+            // Actualizar el voto después de comprobar
+            return await db.collection( COLLECTIONS.VOTES ).updateOne(
+
+                { id },
+                {
+                    $set: { character }
+                }
+
+            ).then( // Caso satisfactorio
+
+                async() => {
+                    return {
+
+                        status: true,
+                        message: 'Voto actualizado correctamente',
+                        characters: getCharacters(db)
+
+                    }
+                }
+
+            ).catch( // Caso falso
+
+                async() => {
+                    return {
+
+                        status: false,
+                        message: 'Voto NO actualizado. Prueba de nuevo',
+                        characters: getCharacters(db)
+
+                    }
+                }
+
+            )
+
+        },
+
+        async deleteVote( _: void, { id }, { db } ) {
+
+            // Comprobar que el voto existe
+            const selectVote = await getVote( db, id );
+            
+            if( selectVote === null || selectVote === undefined )  {
+
+                return {
+
+                    status: false,
+                    message: 'El voto introducido no existe y no puedes borrarlo',
+                    characters: await getCharacters( db )
+
+                }
+
+            }
+
+
+            // Si existe, borrarlo
+            return await db.collection( COLLECTIONS.VOTES ).deleteOne( { id } )
+                .then( // Respuesta satisfactoria
+
+                    async() => {
+                        return {
+                            status: true,
+                            message: 'Voto borrado correctamente',
+                            characters: getCharacters( db )
+                        }
+                    }
+
+                ).catch( // En caso de error
+
+                    async() => {
+                        return {
+                            status: false,
+                            message: 'Voto NO borrado. Inténtelo de nuevo',
+                            characters: getCharacters( db )
+                        }
+                    }
+
+                );
+
+        }
+
+    }
 
 };
 
