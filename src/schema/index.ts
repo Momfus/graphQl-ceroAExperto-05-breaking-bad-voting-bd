0f9ce777bab9f1eb47fe7041b24adb94dac47844
Momfus <momfus@outlook.com>
2020-12-12 23:24:15 -0300

    Modulización de grapqhl - clase 297

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,14 @@
 import { GraphQLSchema } from 'graphql';
 import 'graphql-import-node';
-import typeDefs from './schema.graphql';
+// import typeDefs from './schema.graphql';
 import resolvers from './../resolvers/resolversMap';
 import { makeExecutableSchema } from 'graphql-tools';
+
+import path from 'path';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+
+const typeDefs = mergeTypeDefs (loadFilesSync(path.join(__dirname, './**/*.graphql') ) ); // Hace referencia a cualqueir directorio de fichero con .graphql
 
 const schema: GraphQLSchema = makeExecutableSchema({
     typeDefs,
